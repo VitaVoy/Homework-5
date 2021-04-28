144fd40c53d697539d426e9c2182dfac3c80cd25
Vitali Voitau <videofag@mail.ru>
2021-04-28 19:07:44 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Homework_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свою Фамилию");
+            string Surname = Console.ReadLine();
+            Console.WriteLine("Введите свое Имя");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Введите свой город проживания");
+            string Gorod = Console.ReadLine();
+            Console.SetCursorPosition(Console.WindowWidth / 2, Console.WindowHeight / 2);
+            Console.WriteLine(Surname + " " + Name + " " + Gorod);
+            Console.ReadLine();
+        }
+    }
+}
